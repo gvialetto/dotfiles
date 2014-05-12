c323ebbcb5e412f0a558d6c1c3b02fb8c6b2560e
Gianni Vialetto <g.vialetto@gmail.com>
2014-05-12 20:00:29 +0200

    Better rainbow-delimiters

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -91,6 +91,17 @@
 ;; RAINBOW DELIMITERS - http://www.emacswiki.org/emacs/RainbowDelimiters
 (require 'rainbow-delimiters)
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+(require 'cl-lib)
+(require 'color)
+(cl-loop
+ for index from 1 to rainbow-delimiters-max-face-count
+ do
+ (let ((face (intern (format "rainbow-delimiters-depth-%d-face" index))))
+   (cl-callf color-saturate-name (face-foreground face) 30)))
+(require 'paren) ; show-paren-mismatch is defined in paren.el
+(set-face-attribute 'rainbow-delimiters-unmatched-face nil
+                    :foreground 'unspecified
+                    :inherit 'show-paren-mismatch)
 ;; CLOJURE - https://github.com/clojure-emacs/clojure-mode
 (require 'clojure-mode)
 (add-hook 'clojure-mode-hook 'enable-paredit-mode)
